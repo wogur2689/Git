8496053473164ad9e64027e2a11b32bfc91fb997
jaehyeokGil <81075170+wogur2689@users.noreply.github.com>
2022-06-05 15:40:19 +0900

    테스트 커밋

--- mainC.c
+++ mainC.c
@@ -1,13 +1,6 @@
 #include <stdio.h>
 
 void main() {
-    /* git merge*/
-    int a = 30;
-    int j = 410;
-    int ios = 5;
-    int as = 5;
-    char name[20] = 'hyeok'; 
+    /* github Desktop*/
     printf("hello world");
-    printf("a의 값 %d", a);
-    printf("j의 값 %d", j);
-}
+ 
